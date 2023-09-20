f9d1c7a1cc0824f44d49d06549c7e7d316a907ea
OliverC-123 <71122249+OliverC-123@users.noreply.github.com>
2023-09-20 14:56:44 +0200

    Add files via upload
    remember to npm i first
    
    run the word database server as described in app.js
    and have fun :)

---
+++ src/components/Modal.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Modal({ isCorrect, solution, turn }) {
+  console.log("Modal");
+
+  // Define a function to reload the page
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className="modal">
+      {isCorrect && (
+        <div>
+          <h1>You Win!</h1>
+          <p className="solution">{solution}</p>
+          <p>You found the word in {turn} guesses</p>
+          <button className="replayButton" onClick={reloadPage}>
+            Play Again
+          </button>
+        </div>
+      )}
+      {!isCorrect && (
+        <div>
+          <h1>Unlucky!</h1>
+          <p className="solution">{solution}</p>
+          <p>Better luck next time</p>
+          <button className="replayButton" onClick={reloadPage}>
+            Play Again
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/components/Wordle.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+
+import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
+
+import Keypad from "./Keypad";
+import keys from "./Keys";
+
+import Modal from "./Modal";
+
+export default function Wordle({ solution }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } =
+    useWordle(solution);
+
+  // useEffect to monitor Keyup
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyup);
+
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup]);
+
+  // useEffect to monitor when game ends
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyup);
+
+    if (isCorrect) {
+      setTimeout(() => setShowModal(true), 2000);
+      window.removeEventListener("keyup", handleKeyup);
+    }
+    if (turn > 5) {
+      setTimeout(() => setShowModal(true), 2000);
+      window.removeEventListener("keyup", handleKeyup);
+    }
+
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup, isCorrect, turn]);
+  {
+    {
+      showModal && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      );
+    }
+  }
+
+  return (
+    <div>
+      <h1>Current Guess - {currentGuess}</h1>
+      <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+      <Keypad keys={keys} usedKeys={usedKeys} />
+      {(isCorrect || turn === 5) && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
+    </div>
+  );
+}
